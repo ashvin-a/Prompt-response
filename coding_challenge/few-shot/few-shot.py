6d77ab9e8b9b439a7c91896eae216f9ecd37f8f2
= <=>
2024-08-05 22:45:24 +0530

    Add few shot response code.

---
+++ coding_challenge/few-shot/few-shot.py
@@ -0,0 +1,58 @@
+import json
+import pandas as pd
+from collections import deque
+
+class DataProcessor:
+    def __init__(self, parameter):
+        self.parameter = parameter
+    
+    def process(self):
+        if isinstance(self.parameter, str):
+            # Check if the string is a JSON string
+            try:
+                json_data = json.loads(self.parameter)
+                if isinstance(json_data, list):
+                    return self.convert_json_to_excel(json_data)
+            except json.JSONDecodeError:
+                # Not a JSON string, check for palindrome
+                return self.is_palindrome(self.parameter)
+        elif isinstance(self.parameter, tuple) and len(self.parameter) == 3:
+            begin_word, end_word, word_list = self.parameter
+            return self.shortest_transformation_sequence(begin_word, end_word, word_list)
+        else:
+            raise ValueError("Unsupported parameter type or incorrect tuple format.")
+    
+    def is_palindrome(self, word):
+        word = word.lower()
+        return word == word[::-1]
+    
+    def convert_json_to_excel(self, json_data):
+        df = pd.DataFrame(json_data)
+        output_file = 'output.xlsx'
+        df.to_excel(output_file, index=False)
+        return output_file
+    
+    def shortest_transformation_sequence(self, begin_word, end_word, word_list):
+        word_set = set(word_list)
+        if end_word not in word_set:
+            return 0
+
+        queue = deque([(begin_word, 1)])
+        while queue:
+            current_word, steps = queue.popleft()
+            if current_word == end_word:
+                return steps
+            
+            for i in range(len(current_word)):
+                for c in 'abcdefghijklmnopqrstuvwxyz':
+                    next_word = current_word[:i] + c + current_word[i+1:]
+                    if next_word in word_set:
+                        queue.append((next_word, steps + 1))
+                        word_set.remove(next_word)
+        
+        return 0
+
+with open("coding_challenge/test_files/test_json.json","r") as f:
+    json_data = f.read()
+processor = DataProcessor(("hot", "cog", ["hog", "dog", "log", "cog"]))
+print(processor.process())
